e354023d98b5664ba39d3b52f32d081ba4ba1712
amin-rm <ramdhaniahmedamine@gmail.com>
2024-11-07 14:52:51 +0100

    fixed code quality

--- foyer/src/test/java/tn/esprit/tpfoyer/TestMockFoyerServiceImpl.java
+++ foyer/src/test/java/tn/esprit/tpfoyer/TestMockFoyerServiceImpl.java
@@ -1,6 +1,5 @@
 package tn.esprit.tpfoyer;
 
-import org.h2.command.dml.MergeUsing;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
